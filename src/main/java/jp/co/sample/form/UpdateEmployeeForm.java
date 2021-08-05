5eb3acb483d98230c0d154db4134ae85c43d76d0
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-05 21:01:20 +0900

    make EmployeeService

---
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -0,0 +1,36 @@
+package jp.co.sample.form;
+
+/**
+ * 従業員情報更新時に使用するフォーム.
+ * 
+ * @author yuyayokoyama
+ *
+ */
+public class UpdateEmployeeForm {
+	/** 従業員ID */
+	private String id;
+	/** 扶養人数 */
+	private String dependentsCount;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDependentsCount() {
+		return dependentsCount;
+	}
+	public void setDependentsCount(String dependentsCount) {
+		this.dependentsCount = dependentsCount;
+	}
+	
+	@Override
+	public String toString() {
+		return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+	}
+	
+	
+
+}
--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -6,6 +6,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sample.repository.EmployeeRepository;
 
+/**
+ * 従業員情報一覧を全件検索する業務処理を行うメソッド
+ * 
+ * @author yuyayokoyama
+ *
+ */
 @Service
 @Transactional
 public class EmployeeService {
@@ -13,9 +19,15 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	
+	/**
+	 * 従業員情報を全件取得する.
+	 * @return 従業員情報全件
+	 */
 	public List<Employee> showList(){
 		return employeeRepository.findAll();
 	}
+	
+	
+	
 
 }
