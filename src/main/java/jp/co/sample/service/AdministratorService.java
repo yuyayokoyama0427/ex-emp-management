4600c02a9404080f09be886629fc55565d00910c
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-05 13:49:47 +0900

    make InsertAdministratorForm

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,11 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.repository.AdministratorRepository;
+
+/**
+ * 
+ * 
+ * @author yuyayokoyama
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
 	
 	@Autowired
-	private AdministratiorRepository administratorRepository;
+	private AdministratorRepository administratorRepository;
 
 }
