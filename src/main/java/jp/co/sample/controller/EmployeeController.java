7c872297495186f85cf139f46e92dec000ae44fd
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-05 21:39:29 +0900

    make EmployeeController

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package jp.co.sample.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
+
+/**
+ * 従業員情報を検索する処理のコントローラー.
+ * 
+ * @author yuyayokoyama
+ *
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@RequestMapping("/showList")
+	public String showList(Model model) {
+		List<Employee> employeeList = employeeService.showList();
+		model.addAttribute("employeeList", employeeList);
+		return  "employee/list.html";
+	}
+
+}
--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package jp.co.sample.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Employee;
 import jp.co.sample.repository.EmployeeRepository;
 
 /**
