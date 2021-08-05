8dc634c4d8ecca737d1c728f60caf9a2e1fe97e7
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-05 10:54:35 +0900

    make domain-Employee

--- src/main/java/jp/co/sample/domain/Employee.java
+++ src/main/java/jp/co/sample/domain/Employee.java
@@ -1,5 +1,164 @@
 package jp.co.sample.domain;
 
+import java.util.Date;
+
+/**
+ * 従業員情報を表すドメイン
+ * 
+ * @author yuyayokoyama
+ */
 public class Employee {
+	/** ID */
+	private Integer id;
+	/** 名前 */
+	private String name;
+	/** 画像 */
+	private String image;
+	/** 性別 */
+	private String gender;
+	/** 入社日 */
+	private java.util.Date hireDate;
+	/** メールアドレス */
+	private String mailaddress;
+	/** 郵便番号 */
+	private String zipCode;
+	/** 住所 */
+	private String address;
+	/** 電話番号 */
+	private String telephone;
+	/** 給料 */
+	private Integer salary;
+	/** 特性 */
+	private String characteristics;
+	/** 扶養人数 */
+	private Integer dependentsCount;
+	
+	// 引数なしコンストラクタ
+	public Employee() {
+	}
+	
+	// コンストラクタ
+	public Employee(Integer id, String name, String image, String gender, Date hireDate, String mailaddress,
+			String zipCode, String address, String telephone, Integer salary, String characteristics,
+			Integer dependentsCount) {
+		this.id = id;
+		this.name = name;
+		this.image = image;
+		this.gender = gender;
+		this.hireDate = hireDate;
+		this.mailaddress = mailaddress;
+		this.zipCode = zipCode;
+		this.address = address;
+		this.telephone = telephone;
+		this.salary = salary;
+		this.characteristics = characteristics;
+		this.dependentsCount = dependentsCount;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public java.util.Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(java.util.Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public String getMailaddress() {
+		return mailaddress;
+	}
+
+	public void setMailaddress(String mailaddress) {
+		this.mailaddress = mailaddress;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+
+	public Integer getDependentsCount() {
+		return dependentsCount;
+	}
+
+	public void setDependentsCount(Integer dependentsCount) {
+		this.dependentsCount = dependentsCount;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender + ", hireDate="
+				+ hireDate + ", mailaddress=" + mailaddress + ", zipCode=" + zipCode + ", address=" + address
+				+ ", telephone=" + telephone + ", salary=" + salary + ", characteristics=" + characteristics
+				+ ", dependentsCount=" + dependentsCount + "]";
+	}
+	
+	
 
 }
