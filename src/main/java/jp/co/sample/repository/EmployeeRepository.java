90c7b561e3473eff2df66da3cf1a5035776da5e3
yuyayokoyama0427 <yokoya_mayuya@icloud.com>
2021-08-06 10:46:25 +0900

    make list.html of employee

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -33,14 +33,14 @@ public class EmployeeRepository {
 		employee.setName(rs.getString("name"));
 		employee.setImage(rs.getString("image"));
 		employee.setGender(rs.getString("gender"));
-		employee.setHireDate(rs.getDate("hireDate"));
-		employee.setMailAddress(rs.getString("mailAddress"));
-		employee.setZipCode(rs.getString("zipCode"));
+		employee.setHireDate(rs.getDate("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
 		employee.setAddress(rs.getString("address"));
-		employee.setTelephone(rs.getString("telepohone"));
+		employee.setTelephone(rs.getString("telephone"));
 		employee.setSalary(rs.getInt("salary"));
 		employee.setCharacteristics(rs.getString("characteristics"));
-		employee.setDependentsCount(rs.getInt("dependentsCount"));
+		employee.setDependentsCount(rs.getInt("dependents_count"));
 		
 		return employee;
 	};
@@ -51,8 +51,8 @@ public class EmployeeRepository {
 	 * @return 従業員一覧情報 入社日の降順で並び替え
 	 */
 	public List<Employee> findAll(){
-		String sql = "SELECT id,name,image,gender,hireDate,mailAddress,zipCode,address,telephone,salary,characterisitcs,dependentsCount FROM "
-				+ TABLE_NAME + " ORDER BY hireDate DESC";
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM "
+				+ TABLE_NAME + " ORDER BY hire_date DESC";
 		
 		List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
 		if(employeeList.size() == 0) {
